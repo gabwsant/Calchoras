5406fb1c81bee24166283e8e4843198b666d51fa
Gabriel Barbosa Santos <gabrielsantos.gbs@outlook.com>
2025-03-01 15:23:50 -0300

    Versão 1.0 da tela de cadastro de template finalizada.

--- src/main/java/calchoras/view/CadastraTemplate.java
+++ src/main/java/calchoras/view/CadastraTemplate.java
@@ -90,21 +90,24 @@ public class CadastraTemplate extends javax.swing.JFrame {
         tfSaida7 = new javax.swing.JTextField();
         tfModelo = new javax.swing.JTextField();
         btnCadastraModelo = new javax.swing.JButton();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(550, 550));
+        setPreferredSize(new java.awt.Dimension(550, 550));
+        setSize(new java.awt.Dimension(550, 550));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowOpened(java.awt.event.WindowEvent evt) {
                 formWindowOpened(evt);
             }
         });
 
-        lblTitulo.setFont(new java.awt.Font("Arial", 0, 20)); // NOI18N
+        lblTitulo.setFont(new java.awt.Font("Arial", 0, 36)); // NOI18N
         lblTitulo.setText("Cálculo de Horas Extras");
 
         jLabel1.setFont(new java.awt.Font("Arial", 0, 18)); // NOI18N
         jLabel1.setText("Modelo");
 
+        spnModelo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         spnModelo.setModel(new javax.swing.SpinnerNumberModel(1, 1, null, 1));
         spnModelo.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
@@ -115,179 +118,180 @@ public class CadastraTemplate extends javax.swing.JFrame {
         Grid.setLayout(new java.awt.GridLayout(8, 5, 15, 14));
         Grid.add(jLabel19);
 
+        jLabel20.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel20.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel20.setText("Entrada");
         Grid.add(jLabel20);
 
+        jLabel21.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel21.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel21.setText("Almoço");
         Grid.add(jLabel21);
 
+        jLabel22.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel22.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel22.setText("Volta almoço");
         Grid.add(jLabel22);
 
+        jLabel23.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel23.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel23.setText("Saída");
         Grid.add(jLabel23);
 
+        jLabel24.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel24.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel24.setText("Domingo");
-        jLabel24.setMaximumSize(null);
-        jLabel24.setMinimumSize(null);
         Grid.add(jLabel24);
 
+        tfEntrada1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada1.setMaximumSize(null);
-        tfEntrada1.setMinimumSize(null);
         Grid.add(tfEntrada1);
 
+        tfAlmoco1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco1.setMaximumSize(null);
-        tfAlmoco1.setMinimumSize(null);
         Grid.add(tfAlmoco1);
 
+        tfVolta1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta1.setMaximumSize(null);
-        tfVolta1.setMinimumSize(null);
         Grid.add(tfVolta1);
 
+        tfSaida1.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida1.setMaximumSize(null);
-        tfSaida1.setMinimumSize(null);
         Grid.add(tfSaida1);
 
+        jLabel25.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel25.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel25.setText("Segunda");
-        jLabel25.setMaximumSize(null);
-        jLabel25.setMinimumSize(null);
         Grid.add(jLabel25);
 
+        tfEntrada2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada2.setMaximumSize(null);
-        tfEntrada2.setMinimumSize(null);
         Grid.add(tfEntrada2);
 
+        tfAlmoco2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco2.setMaximumSize(null);
-        tfAlmoco2.setMinimumSize(null);
         Grid.add(tfAlmoco2);
 
+        tfVolta2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta2.setMaximumSize(null);
-        tfVolta2.setMinimumSize(null);
         Grid.add(tfVolta2);
 
+        tfSaida2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida2.setMaximumSize(null);
-        tfSaida2.setMinimumSize(null);
         Grid.add(tfSaida2);
 
+        jLabel26.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel26.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel26.setText("Terça");
         Grid.add(jLabel26);
 
+        tfEntrada3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada3.setMaximumSize(null);
-        tfEntrada3.setMinimumSize(null);
         Grid.add(tfEntrada3);
 
+        tfAlmoco3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco3.setMaximumSize(null);
-        tfAlmoco3.setMinimumSize(null);
         Grid.add(tfAlmoco3);
 
+        tfVolta3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta3.setMaximumSize(null);
-        tfVolta3.setMinimumSize(null);
         Grid.add(tfVolta3);
 
+        tfSaida3.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida3.setMaximumSize(null);
-        tfSaida3.setMinimumSize(null);
         Grid.add(tfSaida3);
 
+        jLabel27.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel27.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel27.setText("Quarta");
-        jLabel27.setMaximumSize(null);
-        jLabel27.setMinimumSize(null);
         Grid.add(jLabel27);
 
+        tfEntrada4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada4.setMaximumSize(null);
-        tfEntrada4.setMinimumSize(null);
         Grid.add(tfEntrada4);
 
+        tfAlmoco4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco4.setMaximumSize(null);
-        tfAlmoco4.setMinimumSize(null);
         Grid.add(tfAlmoco4);
 
+        tfVolta4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta4.setMaximumSize(null);
-        tfVolta4.setMinimumSize(null);
         Grid.add(tfVolta4);
 
+        tfSaida4.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida4.setMaximumSize(null);
-        tfSaida4.setMinimumSize(null);
         Grid.add(tfSaida4);
 
+        jLabel28.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel28.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel28.setText("Quinta");
         Grid.add(jLabel28);
 
+        tfEntrada5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada5.setMaximumSize(null);
-        tfEntrada5.setMinimumSize(null);
         Grid.add(tfEntrada5);
 
+        tfAlmoco5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco5.setMaximumSize(null);
-        tfAlmoco5.setMinimumSize(null);
         Grid.add(tfAlmoco5);
 
+        tfVolta5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta5.setMaximumSize(null);
-        tfVolta5.setMinimumSize(null);
         Grid.add(tfVolta5);
 
+        tfSaida5.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida5.setMaximumSize(null);
-        tfSaida5.setMinimumSize(null);
         Grid.add(tfSaida5);
 
+        jLabel29.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel29.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel29.setText("Sexta");
         Grid.add(jLabel29);
 
+        tfEntrada6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada6.setMaximumSize(null);
-        tfEntrada6.setMinimumSize(null);
         Grid.add(tfEntrada6);
 
+        tfAlmoco6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco6.setMaximumSize(null);
-        tfAlmoco6.setMinimumSize(null);
         Grid.add(tfAlmoco6);
 
+        tfVolta6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta6.setMaximumSize(null);
-        tfVolta6.setMinimumSize(null);
         Grid.add(tfVolta6);
 
+        tfSaida6.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida6.setMaximumSize(null);
-        tfSaida6.setMinimumSize(null);
         Grid.add(tfSaida6);
 
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
         jLabel2.setText("Sábado");
         Grid.add(jLabel2);
 
+        tfEntrada7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfEntrada7.setMaximumSize(null);
-        tfEntrada7.setMinimumSize(null);
         Grid.add(tfEntrada7);
 
+        tfAlmoco7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfAlmoco7.setMaximumSize(null);
-        tfAlmoco7.setMinimumSize(null);
         Grid.add(tfAlmoco7);
 
+        tfVolta7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfVolta7.setMaximumSize(null);
-        tfVolta7.setMinimumSize(null);
         Grid.add(tfVolta7);
 
+        tfSaida7.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         tfSaida7.setMaximumSize(null);
-        tfSaida7.setMinimumSize(null);
         Grid.add(tfSaida7);
 
+        tfModelo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
+
+        btnCadastraModelo.setFont(new java.awt.Font("Segoe UI", 0, 14)); // NOI18N
         btnCadastraModelo.setText("Cadastra");
         btnCadastraModelo.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnCadastraModeloActionPerformed(evt);
-            }
-        });
-
-        jButton1.setText("jButton1");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
             }
         });
 
@@ -297,39 +301,35 @@ public class CadastraTemplate extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(lblTitulo)
-                    .addComponent(Grid, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(12, 12, 12)
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addGroup(layout.createSequentialGroup()
-                                .addComponent(spnModelo, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(tfModelo, javax.swing.GroupLayout.PREFERRED_SIZE, 193, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(btnCadastraModelo))
-                            .addComponent(jButton1))))
-                .addContainerGap(37, Short.MAX_VALUE))
+                    .addComponent(Grid, javax.swing.GroupLayout.PREFERRED_SIZE, 503, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(35, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addComponent(jLabel1)
+                .addGap(18, 18, 18)
+                .addComponent(spnModelo, javax.swing.GroupLayout.PREFERRED_SIZE, 56, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tfModelo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnCadastraModelo)
+                .addGap(16, 16, 16))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(lblTitulo)
-                .addGap(18, 18, 18)
+                .addGap(24, 24, 24)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel1)
                     .addComponent(spnModelo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(tfModelo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(btnCadastraModelo))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(Grid, javax.swing.GroupLayout.PREFERRED_SIZE, 304, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(Grid, javax.swing.GroupLayout.DEFAULT_SIZE, 367, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         pack();
@@ -380,24 +380,34 @@ public class CadastraTemplate extends javax.swing.JFrame {
 }
     
     private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
-        //Define ícone da janela
         URL imageUrl = CadastraTemplate.class.getClassLoader().getResource("calc_icon.png");
         setIconImage(new ImageIcon(imageUrl).getImage());
+        atualizaHorariosMostrados();
     }//GEN-LAST:event_formWindowOpened
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        TemplateModel template = TemplateModel.buscarTemplatePorModelo(1);
-        System.out.print(template.getEmpresa());
-    }//GEN-LAST:event_jButton1ActionPerformed
-
     private void spnModeloStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_spnModeloStateChanged
-        
+        atualizaHorariosMostrados();
+    }//GEN-LAST:event_spnModeloStateChanged
+     
+    public void atualizaHorariosMostrados(){
         try{
+            int index = 0;
+            int linha = index / 4;  // Cada linha tem 4 campos (entrada, almoço, volta, saída)
+            int coluna = index % 4; // Define qual tipo de horário está sendo preenchido 
             int modeloSelecionado = (int) spnModelo.getValue();
             TemplateModel templateAtual = TemplateController.buscarTemplate(modeloSelecionado);
         
             if (templateAtual == null) {
                 System.out.println("Não existe modelo salvo com o número " + modeloSelecionado);
+                Component[] componentes = Grid.getComponents();
+                tfModelo.setText("");
+
+                for (Component c : componentes) {
+                    if (c instanceof JTextField) {                   
+                        ((JTextField) c).setText("");
+                    index++; 
+                    }
+                }
                 return;
             }
         
@@ -405,13 +415,9 @@ public class CadastraTemplate extends javax.swing.JFrame {
             String[][] horarios = TemplateController.getHorarios(templateAtual);
         
             Component[] componentes = Grid.getComponents();
-            int index = 0;
         
             for (Component c : componentes) {
                 if (c instanceof JTextField) {
-                    int linha = index / 4;  // Cada linha tem 4 campos (entrada, almoço, volta, saída)
-                    int coluna = index % 4; // Define qual tipo de horário está sendo preenchido
-                
                     if (linha < horarios.length && coluna < horarios[linha].length) {
                         ((JTextField) c).setText(horarios[linha][coluna]);
                     }
@@ -422,12 +428,10 @@ public class CadastraTemplate extends javax.swing.JFrame {
         } catch (Exception e) {
             System.out.println("Erro ao buscar modelo: " + e.getMessage());
         }
-    }//GEN-LAST:event_spnModeloStateChanged
-           
+    }   
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JPanel Grid;
     private javax.swing.JButton btnCadastraModelo;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel19;
     private javax.swing.JLabel jLabel2;
